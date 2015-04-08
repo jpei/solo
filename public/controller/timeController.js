111b13b07d185cfb17dd4e4a81976729ca7c77f1
Joseph Pei <pei_joseph@yahoo.com>
2015-04-07 23:11:08 -0700

    Testing long polling

--- public/controller/timeController.js
+++ public/controller/timeController.js
@@ -1,6 +1,6 @@
 angular.module('crime-stats.timeController', [])
 
-.controller('TimeController', ['$scope', '$http', '$location', 'TimeUnits', function ($scope, $http, $location, TimeUnits) {
+.controller('TimeController', ['$scope', '$http', '$location', '$window', 'TimeUnits', 'AnalyzeCrimes', function ($scope, $http, $location, $window, TimeUnits, AnalyzeCrimes) {
   $scope.crimes = [];
   $scope.count = $scope.frequency = 0;
   $scope.timeUnit = $location.$$url.slice(1);
@@ -16,56 +16,69 @@ angular.module('crime-stats.timeController', [])
     return str.charAt(0).toUpperCase()+str.slice(1);
   };
 
-  $http.get('/crime/'+$scope.timeUnit)
-    .success(function(data) {
-      console.log('ON SUCCESS');
-      if (!Array.isArray(data)) {
-        console.error('Data is not parseable');
-        return;
-      }
-      var newCrimes = data;
-      $scope.crimes = $scope.crimes.concat(newCrimes);
-      $scope.count = $scope.crimes.length;
-      $scope.frequency = ($scope.count / (TimeUnits[$scope.timeUnit]() / TimeUnits.day())).toString().slice(0,7);
+  var createXMLHttpRequest = function() {
+    try { return new $window.XMLHttpRequest();                   } catch(e) {}
+    try { return new $window.ActiveXObject("Microsoft.XMLHTTP"); } catch(e) {}
+    try { return new $window.ActiveXObject("Msxml2.XMLHTTP");    } catch(e) {}
+    $window.alert("XMLHttpRequest not supported");
+    return null;
+  };
 
-      // Count crimes by type
+  var handleResponse = function() {
+    if (http.readyState !== 4 && http.readyState !== 3)
+      return;
+    if (http.readyState === 3 && http.status !== 200)
+      return;
+    if (http.readyState === 4 && http.status !== 200) {
+      clearInterval(pollTimer);
+      inProgress = false;
+    }
+    // In konqueror http.responseText is sometimes null here...
+    if (http.responseText === null)
+      return;
 
-      for (var i=0; i<newCrimes.length; i++) {
-        switch(newCrimes[i][2].toLowerCase()) {
-          case 'aggravated assault':
-          case 'murder':
-          case 'robbery':
-          case 'simple assault':
-            $scope.type.violent++;
-            break;
-          case 'disturbing the peace':
-          case 'narcotics':
-          case 'alcohol':
-          case 'prostitution':
-            $scope.type.property++;
-            break;
-          case 'theft':
-          case 'vehicle theft':
-          case 'vandalism':
-          case 'burglary':
-          case 'arson':
-            $scope.type['quality of life']++;
-            break;
-          default:
-            $scope.type.other++;
-        }
-      }
+    // while (prevDataLength !== http.responseText.length) {
+    //   if (http.readyState === 4  && prevDataLength === http.responseText.length)
+    //     break;
+    //   prevDataLength = http.responseText.length;
+    //   var response = http.responseText.substring(nextLine);
+    //   var lines = response.split('\n');
+    //   nextLine = nextLine + response.lastIndexOf('\n') + 1;
+    //   if (response[response.length-1] !== '\n')
+    //     lines.pop();
 
-      // Count crimes by day of week
-      if (TimeUnits[$scope.timeUnit]()>=TimeUnits.week()) {
-        for (var i=0; i<newCrimes.length; i++) {
-          $scope.dayOfWeek[new Date(newCrimes[i][0].split('T')[0]).getUTCDay()]++; // Slice off end to suppress time zones
-        }
-      }
-    })
-    .error(function(data, status) {
-      console.error('Error, Status Code: ', status || 500);
-    });
+    //   for (var i = 0; i < lines.length; i++) {
+    //       // ...
+    //   }
+    // }
+    console.log(http.responseText.length);
+    // http.responseText = '';
+
+    if (http.readyState === 4 && prevDataLength === http.responseText.length)
+      clearInterval(pollTimer);
+
+    inProgress = false;
+  };
+
+  var http = createXMLHttpRequest();
+  http.open('GET', '/crime/'+$scope.timeUnit, true);
+  http.onreadystatechange = handleResponse;
+  http.send();
+  var pollTimer = setInterval(handleResponse, 1000);
+  var inProgress = true;
+
+  // $http.get('/crime/'+$scope.timeUnit)
+  //   .success(function(data) {
+  //     console.log('ON SUCCESS');
+  //     if (!Array.isArray(data)) {
+  //       console.error('Data is not parseable');
+  //       return;
+  //     }
+  //     AnalyzeCrimes.analyzeAll(data, $scope, TimeUnits);
+  //   })
+  //   .error(function(data, status) {
+  //     console.error('Error, Status Code: ', status || 500);
+  //   });
 }])
 .factory('TimeUnits', function () {
   var hour = function() {
@@ -90,6 +103,49 @@ angular.module('crime-stats.timeController', [])
     month: month,
     year: year
   };
+})
+.factory('AnalyzeCrimes', function() {
+  var analyzeAll = function(data, $scope, TimeUnits) {
+    var newCrimes = data;
+    $scope.crimes = $scope.crimes.concat(newCrimes);
+    $scope.count = $scope.crimes.length;
+    $scope.frequency = ($scope.count / (TimeUnits[$scope.timeUnit]() / TimeUnits.day())).toString().slice(0,7);
+
+    // Count crimes by type
+    for (var i=0; i<newCrimes.length; i++) {
+      switch(newCrimes[i][2].toLowerCase()) {
+        case 'aggravated assault':
+        case 'murder':
+        case 'robbery':
+        case 'simple assault':
+          $scope.type.violent++;
+          break;
+        case 'disturbing the peace':
+        case 'narcotics':
+        case 'alcohol':
+        case 'prostitution':
+          $scope.type.property++;
+          break;
+        case 'theft':
+        case 'vehicle theft':
+        case 'vandalism':
+        case 'burglary':
+        case 'arson':
+          $scope.type['quality of life']++;
+          break;
+        default:
+          $scope.type.other++;
+      }
+    }
+
+    // Count crimes by day of week
+    if (TimeUnits[$scope.timeUnit]()>=TimeUnits.week()) {
+      for (var j=0; j<newCrimes.length; j++) {
+        $scope.dayOfWeek[new Date(newCrimes[j][0].split('T')[0]).getUTCDay()]++; // Slice off end to suppress time zones
+      }
+    }
+  };
+  return { analyzeAll : analyzeAll };
 })
 .directive('loading', ['$http', function ($http) {
   return {
